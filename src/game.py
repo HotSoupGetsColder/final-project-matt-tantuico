378dd8480d121b73dcfe63be8571e2bd7822e744
Saucy Stew <mtantuico1@ucvts.org>
2019-12-29 12:28:36 -0500

    added way to display custom centered text

--- src/game.py
+++ src/game.py
@@ -4,19 +4,31 @@ import pygame
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
-# Create a font object
-font = pygame.font.SysFont("Cambria", 20)
-# Create text using the font
-text = font.render("Bagels on a Sunday", True, (0,0,0))
+
+# Screen size parameters
+display_width, display_height = 800, 600
+
+
+# Returns text as a surface and a rectangle
+def text_objects(text, color, font_type, size):
+    font = pygame.font.SysFont(font_type, size)
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
+# Displays text at center of screen
+def message_to_screen(msg,color, font_type="Cambria", size=20):
+    textSurf, textRect = text_objects(msg, color, font_type, size)
+    textRect.center = (display_width / 2), (display_height / 2)
+    screen.blit(textSurf, textRect)
 
 
 # Create a display. Size must be a tuple, which is why it's in parentheses
-screen = pygame.display.set_mode( (400, 300) )
+screen = pygame.display.set_mode( (display_width, display_height) )
 
 # fill the screen with white
 screen.fill( (255,255,255) )
 # draw text to screen
-screen.blit(text, (200, 150) )
+message_to_screen("Bagels on a Sunday", (0,0,0), "Times New Roman", 30)
 # update the display
 pygame.display.flip()
 
